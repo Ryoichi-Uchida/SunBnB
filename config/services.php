20065dde5eff2a91919ed8d7e95d379b7982f25f
Ryoichi-Uchida <ryo03_uchi_ec_sephi_lipse_loto@yahoo.co.jp>
2019-09-02 16:53:27 +0800

    Making facebook auth function

--- config/services.php
+++ config/services.php
@@ -40,4 +40,11 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT'),
+    ]
+        
+
 ];
---
+++ database/migrations/2019_08_30_100002_create_social_accounts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedinteger('user_id')->nullable();
+            $table->string('provider_user_id');
+            $table->string('provider');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('social_accounts', function (Blueprint $table){
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('social_accounts');
+    }
+}
